ce7a45a022654eb2bd01606e194e1e051e23e265
Sofia Styf <sofia.styf@masarinconsulting.se>
2019-11-18 11:42:50 +0100

    can now see the html from snowdepth usergui under matvarden and nyamatvarden

---
+++ pages/matvarden.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-card>
+    <NuxtLink :to="'/matvarden'" />
+    <span v-html="stuff"></span>
+  </v-card>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData () {
+    const { data } = await axios.get('http://localhost:8880/fetchdata')
+    return { stuff: data }
+  }
+}
+</script>
---
+++ pages/nyamatvarden.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-card>
+    <NuxtLink :to="'/nyamatvarden'" />
+    <span v-html="stuff"></span>
+  </v-card>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData () {
+    const { data } = await axios.get('http://localhost:8880/counter')
+    return { stuff: data }
+  }
+}
+</script>
