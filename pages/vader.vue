c37e9faf0c9e24648bdb4595b31b5cbea990a5e1
Sofia Styf <sofia.styf@masarinconsulting.se>
2019-12-09 18:23:45 +0100

    testing a thing

--- pages/vader.vue
+++ pages/vader.vue
@@ -2,7 +2,7 @@
   <v-layout>
     <v-flex class="text-center">
       <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
+        &#8220;First, solve the problem. Then, write the code. &#8221;
         <footer>
           <small>
             <em>&mdash;John Johnson</em>
