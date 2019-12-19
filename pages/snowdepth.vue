19a6f81b9ab5d3e4da072b0ea76d23968ecf5211
Sofia Styf <sofia.styf@masarinconsulting.se>
2019-12-19 14:00:53 +0100

    map works, with location marker

--- pages/snowdepth.vue
+++ pages/snowdepth.vue
@@ -2,8 +2,10 @@
   <v-container>
     <div id="map-wrap" style="height: 100vh">
       <client-only>
-        <l-map>
+        <l-map :zoom="13" :center="center">
           <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" />
+          <l-marker :lat-lng="center" />
+          <l-circle :lat-lng="center" :radius="radius" />
         </l-map>
       </client-only>
     </div>
@@ -22,30 +24,40 @@
 <script>
 
 export default {
+  data () {
+    return {
+      center: [51.505, -0.09],
+      radius: 0
+    }
+  },
   mounted () {
     const L = this.$L
-    const mymap = this.$L.map('map-wrap')
+    const mymap = L.map('map-wrap')
+
+    const component = this
 
     function onLocationFound (e) {
-      const radius = e.accuracy
+      // const radius = e.accuracy
       console.log(e)
+      component.center = e.latlng
+      component.radius = e.accuracy
+      // L.marker(e.latlng).addTo(mymap)
+      //   .bindPopup('You are within ' + radius + ' meters from this point').openPopup()
 
-      L.marker(e.latlng).addTo(mymap)
-        .bindPopup('You are within ' + radius + ' meters from this point').openPopup()
-
-      L.circle(e.latlng, radius).addTo(mymap)
+      // L.circle(e.latlng, radius).addTo(mymap)
+      console.log(component)
       console.log('locationfound')
     }
-
     mymap.on('locationfound', onLocationFound)
 
     mymap.locate({ setView: true, watch: true, enableHighAccuracy: true })
-    console.log(mymap)
-    console.log(this.$L)
+    // console.log(mymap)
+    // console.log(L)
   }
 }
 
 </script>
 
 <style>
+
 </style>
