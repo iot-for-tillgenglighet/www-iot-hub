8a768dd8f667e0cea448e9291391c65afb19565b
Sofia Styf <sofia.styf@masarinconsulting.se>
2020-01-29 13:46:23 +0100

    matvarden map now behaves as expected

--- pages/matvarden.vue
+++ pages/matvarden.vue
@@ -1,12 +1,13 @@
 <template>
-  <v-container
-    class="mx-auto text-center"
-  >
-    <div id="map-wrap" class="mx-auto text-center">
+  <v-container>
+    <div id="map" class="mx-auto text-center">
       <client-only>
         <l-map :zoom="13" :center="center" style="height: 80vh; width: 80vw;">
           <l-tile-layer url="https://{s}.tile.osm.org/{z}/{x}/{y}.png" />
           <l-marker :lat-lng="center" />
+          <l-circle :lat-lng="center" :radius="radius" />
+          <l-marker :lat-lng="[62.398659, 17.341804]" />
+          <l-marker :lat-lng="[62.379448, 17.345182]" />
         </l-map>
       </client-only>
     </div>
@@ -14,62 +15,28 @@
 </template>
 
 <script>
-import axios from 'axios'
-
 export default {
   data () {
     return {
       center: [62.3908, 17.3069],
-      posLat: 0,
-      posLon: 0
+      radius: 0
     }
   },
   mounted () {
     const L = this.$L
-    const mymap = L.map('map-wrap')
+    const mymap = L.map('map')
 
     const component = this
 
     function onLocationFound (e) {
       component.center = e.latlng
       component.radius = e.accuracy
-
-      component.posLat = e.latlng.lat
-      component.posLon = e.latlng.lng
+      console.log('hej')
     }
+
     mymap.on('locationfound', onLocationFound)
 
     mymap.locate({ setView: true, watch: true, enableHighAccuracy: true })
-  },
-  methods: {
-    getData () {
-      axios({
-        method: 'POST',
-        url: 'http://localhost:8282/api/graphql',
-        data: {
-          query: `
-            {
-            snowdepths {
-              from {
-                pos {
-                  lat
-                  lon
-                }
-              }
-              depth
-              when
-            }
-          }
-        `
-        }
-      }).then(
-        (result) => {
-          console.log(result.data)
-        }, (error) => {
-          console.error(error)
-        }
-      )
-    }
   }
 }
 </script>
