e0388612dd9313f14983ce164bf4020e0f80148e
Sofia Styf <sofia.styf@masarinconsulting.se>
2020-02-13 15:00:50 +0100

    save point - prepared test case for polling

--- pages/matvarden.vue
+++ pages/matvarden.vue
@@ -7,6 +7,8 @@
 
 <script>
 import axios from 'axios'
+import Snowdepth from '../components/models/snowdepth.model.js'
+import MeasurementPosition from '../components/models/measurementPosition.model.js'
 
 export default {
   mounted () {
@@ -21,24 +23,55 @@ export default {
 
     newmap.setView([62.3908, 17.3069], 12)
 
-    axios({
-      method: 'GET',
-      url: process.env.baseUrl + '/api/graphql?query={snowdepths{from{pos{lat,lon}},when,depth,manual}}'
-    }).then(
-      (result) => {
-        const results = result.data.data.snowdepths
-        for (let i = 0; i < results.length; i++) {
-          const latlng = { lat: results[i].from.pos.lat, lon: results[i].from.pos.lon }
-          const depths = Math.round(((results[i].depth) + Number.EPSILON) * 100) / 100
-          const classLabelName = (results[i].manual === true ? 'manualPopup' : 'sensorPopup')
+    function fetchData(){
+      axios({
+        method: 'GET',
+        url: process.env.baseUrl + '/api/graphql?query={snowdepths{from{pos{lat,lon}},when,depth,manual}}'
+      }).then(
+        (result) => {
+          placePopups(result)
+        }
+      )
+    }
 
-          L.popup({ autoClose: false, closeOnClick: false, closeButton: false, closeOnEscapeKey: false, className: classLabelName, autoPan: false })
-            .setLatLng(latlng)
-            .setContent(depths + ' cm')
-            .openOn(newmap)
+    function placePopups (result) {
+      const results = result.data.data.snowdepths
+
+      for (let i = 0; i < results.length; i++) {
+        const latlng = { lat: results[i].from.pos.lat, lon: results[i].from.pos.lon }
+        const depths = Math.round(((results[i].depth) + Number.EPSILON) * 100) / 100
+        const classLabelName = (results[i].manual === true ? 'manualPopup' : 'sensorPopup')
+
+        L.popup({ autoClose: false, closeOnClick: false, closeButton: false, closeOnEscapeKey: false, className: classLabelName, autoPan: false })
+          .setLatLng(latlng)
+          .setContent(depths + ' cm')
+          .openOn(newmap)
+      }
+    }
+
+    function tryTest() {
+        setTimeout(function () {
+            testData();
+            tryTest();
+        }, 1000);
+    }
+
+    function testData(){
+
+      console.log('testData')
+
+      let randomnumber = Math.floor(Math.random() * (25 - 1 + 1)) + 1;
+      
+      let stuff = new Snowdepth(new MeasurementPosition(62.3901, 17.3062), "2020-02-13T10:20:22Z", randomnumber, true);
+      let data = {
+        data: {
+          data: {
+            snowdepths: [stuff]
+          }
         }
       }
-    )
+      placePopups(data)
+    }
   }
 }
 </script>
